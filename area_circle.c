918fcb0147e1b97e8985e4a4e948cce416bd4d55
AnuragKuila <ajdjsjsjsjsjsj959@gmail.com>
2025-01-22 22:34:19 +0530

    Update and rename area_circle (1).c to area_circle.c

--- area_circle.c
+++ area_circle.c
@@ -3,9 +3,9 @@ int main()
 {
 	int r;
 	float area;
-	printf("Enter the radius ");
+	printf("Enter the radius: ");
 	scanf("%d",&r);
 	area = 3.14*r*r;
-	printf("The area of the circle is = %f",area);
+	printf("The area of the circle is = %.2f\n",area);
 	return 0;
 }
