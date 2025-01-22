e6d2e1d874eb2055c1ae5722b547f845b2380c6d
AnuragKuila <ajdjsjsjsjsjsj959@gmail.com>
2025-01-22 22:28:21 +0530

    Update addition.c

--- addition.c
+++ addition.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-int main () {
+int main () 
+{
   int a = 50, b = 20;
   int c = a + b;
-  printf( "The value of the addition is\n");
-  printf("%d %d %d\n", a, b, c);
+  printf( "The addition of two numbers\n");
+  printf("1.The value of A is:%d\n",a);
+  printf("2.The Value of B is:%d\n",b );
+  printf("3.The Sum of A+B is:%d\n",c);
   return 0;
 }
