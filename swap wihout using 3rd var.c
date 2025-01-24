4a7e0b9fc106ec83f57f9c61fc9db5b2a01e0485
AnuragKuila <ajdjsjsjsjsjsj959@gmail.com>
2025-01-24 10:04:25 +0530

    Update swap wihout using 3rd var.c

--- swap wihout using 3rd var.c
+++ swap wihout using 3rd var.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
-int main() 
+int main()
 {
-	int a,b;
+	float a,b;
 	printf("Enter the 1st value A= ");
-	scanf("%d",&a);
+	scanf("%f",&a);
 	printf("Enter the 2nd value B= ");
-	scanf("%d",&b);
+	scanf("%f",&b);
 	a=a+b;
 	b=a-b;
 	a=a-b;
-	printf("After swap,value of A=%d\n",a);
-	printf("After swap,value of B=%d\n",b);
-	
+	printf("After swap,value of A= %.f\n",a);
+	printf("After swap,value of B= %.f\n",b);
 	return 0;
 }
