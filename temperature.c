d994efc2ba42b987186d19bc40034e12d21c09ea
AnuragKuila <ajdjsjsjsjsjsj959@gmail.com>
2025-01-24 10:11:24 +0530

    Update temperature.c

--- temperature.c
+++ temperature.c
@@ -1,14 +1,15 @@
 #include<stdio.h>  
 int main() 
 {   
-   float fahrenheit,celsius;  
-   printf("Enter the temp in celsius =");
-   scanf("%f",&celsius);
-  fahrenheit=((celsius*9)/5)+32;  
-   printf("Temperature in fahrenheit is:  %f",fahrenheit);
-   printf("\n\n Enter the temp in fahrenheit =");
-   scanf("%f",&fahrenheit);
-   celsius=((fahrenheit-32/9)*5);
-    printf("Temperature in celsius is:  %f",celsius);
+   float f1,c1,f2,c2;  
+   printf("Enter the temp in celsius= ");
+   scanf("%f",&c2);
+  f1=(c2*9/5)+32;  
+   printf("Temperature in fahrenheit is: %.2f\n",f1);
+   printf("Enter the temp in fahrenheit= ");
+   scanf("%f",&f2);
+   c1=(f2-32)*5/9;
+    printf("Temperature in celsius is: %.2f\n",c1);
     return (0);  
     }
+    
