d0795b8a90bf1cac040eb5b8d7471c0aeadf5aeb
AnuragKuila <ajdjsjsjsjsjsj959@gmail.com>
2025-03-22 13:24:29 +0530

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -3,13 +3,13 @@
 int main() {
 	int i,j,k,c=1; 
     
-    for (int i = 1; i <= 4; i++) {
+    for ( i = 1; i <= 4; i++) {
         
-        for (int j = 1; j <= 4 - i; j++) {
+        for ( j = 1; j <= 4 - i; j++) {
             printf(" ");
         }
         
-        for (int j = 1; j <= i; j++) {
+        for ( j = 1; j <= i; j++) {
             printf("%d", c);
          c++; 
         }
